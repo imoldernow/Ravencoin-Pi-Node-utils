f76493e925c73959b6b11c290ad28af931f439b0
imoldernow <kevin@zsenak.com>
2021-09-11 10:24:59 -0400

    Initial commit of check_status and connectedhosts

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Ravencoin-Pi-Node-utils
 Utilities to use with your Ravencoin Pi Node
+
+check_status.sh
+In the file credit is given to the original authors and this version has a slight tweek by me.
+This produces some basic information such as how long ravend has been running and how far behind it is from the current block.
+The difference should not be more than a few minutes. If it is, check ~/.raven/debug.log for possible errors.
+
+connectedhosts.sh
+This shows the IP addresses and ports of hosts that are connected to ravend. 
+IP addresses that show a port different than 8767 are inbound connections.
---
+++ connectedhosts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script generates a list of ip addresses connected to ravend
+# This script assumes ranvend is running and rpc is enabled on localhost port 8766
+# ravend will generate ~/.raven/.cookie if server=1 and rpcuser/rpcpassword are not set
+# This script assumes ~/.raven/.cookie exists
+
+# The cookie file doesn't have a line feed so we will make a file that we can read
+cp ~/.raven/.cookie ~/.raven/.cleancookie
+# Add new line
+echo >> ~/.raven/.cleancookie
+
+#Get the id and password from the cookie
+while IFS=: read -r USER PASSWORD
+do
+   # get the connected hosts
+   curl --user $USER:$PASSWORD --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getpeerinfo", "params": [] }' -H 'content-type: text/plain;' http://127.0.0.1:8766/ 2>/dev/null | grep -o '"addr":"[^"]\+"' | cut -d":" -f2-3 | sed 's/^"//;s/"$//'
+done < ~/.raven/.cleancookie
+
